bf2c7512fb7219cf205d38e6967f3468700eb85e
Davit Margaryan <dmargaryan88@gmail.com>
2021-11-20 21:48:20 +0400

    upload

---
+++ ListOfWeekDays.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFunctions
+{
+    class ListOfWeekDays
+    {
+        private string[] _items = new string[7] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(_items, _items.Length);
+        }
+
+        public class Enumerator
+        {
+            private int _size;
+            private int _count = 0;
+            private string[] _items;
+
+            public Enumerator(string[] items, int size)
+            {
+                _items = items;
+                _size = size;
+            }
+
+            public object Current { get => _items[_count++]; }
+            public bool MoveNext()
+            {
+                return _count < _size;
+            }
+            
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _List _list = new _List();
+            _list.Add(10);
+            _list.Add(11);
+            _list.Add(12);
+            _list.Add(13);
+            _list.Add(14);
+
+            _list.Insert(3, 36);    
+
+            foreach(var item in _list)
+            {
+                Console.WriteLine(item);
+            }
+
+            ListOfWeekDays days = new ListOfWeekDays();
+            foreach  (string item in days)
+            {
+                Console.WriteLine(item);
+            }
+        
+           
+            Console.ReadKey();
+        }
+    }
+}
